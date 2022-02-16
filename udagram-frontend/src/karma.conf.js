dc02f94cf53fadf5be5a26b173fbf5c175903ba8
mostafaitalian <eng_mustafa_yossef@hotmail.com>
2022-02-16 03:06:38 +0200

    xyz

--- udagram-frontend/src/karma.conf.js
+++ udagram-frontend/src/karma.conf.js
@@ -12,17 +12,17 @@ module.exports = function (config) {
       require('karma-coverage-istanbul-reporter'),
       require('@angular-devkit/build-angular/plugins/karma')
     ],
-    // browsers: ['ChromeHeadless_test'],
-    // customLaunchers: {
-    //   ChromeHeadless_test: {
-    //     base: 'ChromeHeadless',
-    //     flags: [
-    //       '--disable-extensions', 
-    //       '--no-sandbox', 
-    //       '--disable-web-security', 
-    //       '--no-proxy-server']
-    //   }
-    // },
+    browsers: ['ChromeHeadless'],
+    customLaunchers: {
+      ChromeHeadless_test: {
+        base: 'ChromeHeadless',
+        flags: [
+          '--disable-extensions', 
+          '--no-sandbox', 
+          '--disable-web-security', 
+          '--no-proxy-server']
+      }
+    },
     client: {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
